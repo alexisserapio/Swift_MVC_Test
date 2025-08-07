c1104cc136d166a5ec6d3fe7c067444c83d927b3
Alexis Serapio <alexis.serapioh@gmail.com>
2025-08-07 15:57:55 -0600

    Se termina el despliegue de los pokemon y se añade el botón de back funcional

--- MVC/Controller/ModalViewController.swift
+++ MVC/Controller/ModalViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 class ModalViewController: UIViewController {
     var pokemon_recibido = Pokemon(image_id: nil, name: nil, movement: nil, skill: nil)
     
-    @IBOutlet weak var pokemonInfoLabel: UILabel!
+    @IBOutlet weak var pokemonNameLabel: UILabel!
     
+    @IBOutlet weak var pokemonImageView: UIImageView!
+    
+    @IBOutlet weak var pokemonMovementLabel: UILabel!
+    
+    @IBOutlet weak var pokemonSkillLabel: UILabel!
+    
+    @IBAction func buttonTappedModal(_ sender: UIButton) {
+        
+        self.performSegue(withIdentifier: "PokemonListSegue", sender: self)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-            
+        
         print("Pokemon recibido: \(pokemon_recibido)")
-            
-        pokemonInfoLabel.text = pokemon_recibido.skill
-            // Do any additional setup after loading the view.
+        
+        pokemonNameLabel.text = pokemon_recibido.name
+        
+        if let imageId = pokemon_recibido.image_id {
+            pokemonImageView.image = UIImage(named: String(describing: imageId))
+        }else{
+            pokemonImageView.image = UIImage(named: "default_image")
         }
+        
+        pokemonMovementLabel.text = pokemon_recibido.movement
+        
+        pokemonSkillLabel.text = pokemon_recibido.skill
+    }
+    
         
         override func viewWillAppear(_ animated: Bool){
             
